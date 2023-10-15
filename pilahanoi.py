1520d57765fe5ebdf13d5af9086a0959a6fcd4e2
Martina Garcia <martinagg@myuax.com>
2023-10-15 23:19:24 +0200

    .

--- pilahanoi.py
+++ pilahanoi.py
@@ -1,46 +1,40 @@
 "Clase NodoPila"
+"Explicacion:en nuestro reto torres de hanoi, cada nodo de la pila representa una configuración de los discos en las torres. Por lo tanto, cada nodo debe guardar el número de discos que hay en cada torre. Yd tener una referencia al siguiente nodo, para poder ir pasando los discos de un nodo a otro."
 class NodoPila:
-    "ATRIBUTOS DE LA CLASE NODOPILA"
-    def __init__(self,numero_discos):
-        self.numero_discos = numero_discos
+    def __init__(self, numero_de_discos):
+        self.numero_de_discos = numero_de_discos
         self.siguiente = None 
 
-"Clase Pila"
 class Pila:
-    "ATRIBUTOS DE LA CLASE PILA"
     def __init__(self):
         self.cima = None 
         self.altura = 0
     
-    " MÉTODO:apilar discos "
-    def push(self,nuevo_numero_discos):
-        nuevo_nodo = NodoPila(nuevo_numero_discos)
+    def push(self, nuevo_numero_de_discos):
+        nuevo_nodo = NodoPila(nuevo_numero_de_discos)
         if self.cima is None:
             self.cima = nuevo_nodo
         else:
             nuevo_nodo.siguiente = self.cima
             self.cima = nuevo_nodo
         self.altura += 1
-    "METODO: desapilar discos"
+
     def pop(self):
-     if self.cima is None:
-        return None
-     else:
-        numero_de_discos = self.cima.numero_de_discos
-        self.cima = self.cima.siguiente
-        self.tamano -= 1
-        return numero_de_discos
+        if self.cima is None:
+            return None
+        else:
+            numero_de_discos = self.cima.numero_de_discos
+            self.cima = self.cima.siguiente
+            self.altura -= 1
+            return numero_de_discos
      
-    "METODO: informacion de cada pila"
     def __str__(self):
         if self.cima is None:
             return "Pila vacía"
         else:
-            valores=[]
+            contenido = []
             nodo_actual = self.cima
             while nodo_actual:
-                valores.append(nodo_actual.numero_discos)
+                contenido.append(nodo_actual.numero_de_discos)
                 nodo_actual = nodo_actual.siguiente
-            return str(valores)
-
-
+            return str(contenido)
